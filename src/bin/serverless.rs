da039269cbc306768a3bb8985de120ec524091b6
Arvid Böttiger <arbo@milestone.dk>
2020-08-28 15:05:49 +0200

    Updated serverless.rs

--- src/bin/serverless.rs
+++ src/bin/serverless.rs
@@ -10,28 +10,5 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn hello(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    // `serde_json::Values` impl `IntoResponse` by default
-    // creating an application/json response
-    Ok(json!({
-        "message": "Go Serverless v1.0! Your function executed successfully!"
-    }))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn hello_handles() {
-        let request = Request::default();
-        let expected = json!({
-            "message": "Go Serverless v1.0! Your function executed successfully!"
-        })
-        .into_response();
-        let response = hello(request, Context::default())
-            .await
-            .expect("expected Ok(_) value")
-            .into_response();
-        assert_eq!(response.body(), expected.body())
-    }
+    Ok(json!({"message": "Go Serverless v1.0! Your function executed successfully!"}))
 }
