343ac47be65cdff3ff9bb0b008dafc7f2bc33676
Arvid Böttiger <bottiger@gmail.com>
2021-09-21 08:58:17 +0200

    Testing Rocket backend to lambda

--- src/bin/lambda.rs
+++ src/bin/lambda.rs
@@ -1,3 +1,4 @@
+/*
 use lambda_http::{handler, lambda_runtime::{self, Context, Error}, IntoResponse, Request, RequestExt};
 
 #[tokio::main]
@@ -17,4 +18,26 @@ async fn hello(
             .get("name")
             .unwrap_or_else(|| "stranger")
     ))
+}
+*/
+
+use rocket::{self, get, routes};
+use lambda_web::{is_running_on_lambda, launch_rocket_on_lambda, LambdaError};
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[rocket::main]
+async fn main() -> Result<(), LambdaError> {
+    let rocket = rocket::build().mount("/", routes![hello]);
+    if is_running_on_lambda() {
+        // Launch on AWS Lambda
+        launch_rocket_on_lambda(rocket).await?;
+    } else {
+        // Launch local server
+        rocket.launch().await?;
+    }
+    Ok(())
 }
