ed1411907e820b283a20d7ecb683399e8adbe235
Arvid Böttiger <bottiger@gmail.com>
2024-06-18 14:47:17 +0200

    Updated dependencies

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -84,16 +84,5 @@ fn parse_args(_args: Vec<String>) -> clap::ArgMatches<'static> {
                 .multiple(true)
                 .help("Sets the level of verbosity"),
         )
-        .subcommand(
-            SubCommand::with_name("test")
-                .about("controls testing features")
-                .version("1.3")
-                .author("Someone E. <someone_else@other.com>")
-                .arg(
-                    Arg::with_name("debug")
-                        .short("d")
-                        .help("print debug information verbosely"),
-                ),
-        )
         .get_matches()
 }
