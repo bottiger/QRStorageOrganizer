2b3e9633dad3ca7e7320300d30ef590930cd0ec4
Arvid Böttiger <bottiger@gmail.com>
2024-12-03 15:41:01 +0100

    Added simple webserver

--- src/bin/rest.rs
+++ src/bin/rest.rs
@@ -1,19 +1,62 @@
-extern crate qrstore;
-extern crate env_logger;
 
+extern crate qrcode;
+
+use actix_web::HttpRequest;
+use actix_web::{HttpResponse, Responder};
+use pnet::ipnetwork::IpNetwork;
+use qrstore::model::qruuid::parse_qr_val;
+use serde_json::json;
 use actix_web::{web, App, HttpServer};
+use std::net::IpAddr;
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-	env_logger::init();
-
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(crate::qrstore::rest::index))
-            .route("/{qr}", web::get().to(crate::qrstore::rest::index2))
+            .route("/ping", web::get().to(ping))
+            .route("/ip", web::get().to(ip))
+			.route("/qr", web::get().to(index2))
     })
-    .bind("0.0.0.0:8080")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+pub async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+pub async fn index2(info: web::Path<String>, _req: HttpRequest) -> impl Responder {
+
+	match parse_qr_val(info.to_string()) {
+		Ok(v) => {
+			let resp_json = json!(v).to_string();
+			HttpResponse::Ok().body(resp_json)
+		},
+		Err(_e) => HttpResponse::BadRequest().body("Unable to parse input")
+	}
+}
+
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+async fn ip() -> impl Responder {
+    match get_ip_address() {
+        Some(ip) => HttpResponse::Ok().body(ip.to_string()),
+        None => HttpResponse::InternalServerError().body("Unable to retrieve IP"),
+    }
+}
+
+fn get_ip_address() -> Option<IpAddr> {
+    // Try to fetch the local IP address of the server
+    let interfaces = pnet::datalink::interfaces();
+    for interface in interfaces {
+        for ip in interface.ips {
+            if let IpNetwork::V4(ipv4) = ip {
+                return Some(IpAddr::V4(ipv4.ip()));
+            }
+        }
+    }
+    None
 }
--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -1,11 +1,26 @@
-/*
+
 extern crate qrcode;
 
-use crate::dynamodb::qruuid::parse_qr_val;
 use actix_web::HttpRequest;
 use actix_web::{HttpResponse, Responder};
+use pnet::ipnetwork::IpNetwork;
 use serde_json::json;
-use actix_web::web;
+use actix_web::{web, App, HttpServer};
+use std::net::IpAddr;
+use crate::model::qruuid::parse_qr_val;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/ping", web::get().to(ping))
+            .route("/ip", web::get().to(ip))
+			.route("/qr", web::get().to(index2))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
 
 pub async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -21,4 +36,27 @@ pub async fn index2(info: web::Path<String>, _req: HttpRequest) -> impl Responde
 		Err(_e) => HttpResponse::BadRequest().body("Unable to parse input")
 	}
 }
-*/
+
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+async fn ip() -> impl Responder {
+    match get_ip_address() {
+        Some(ip) => HttpResponse::Ok().body(ip.to_string()),
+        None => HttpResponse::InternalServerError().body("Unable to retrieve IP"),
+    }
+}
+
+fn get_ip_address() -> Option<IpAddr> {
+    // Try to fetch the local IP address of the server
+    let interfaces = pnet::datalink::interfaces();
+    for interface in interfaces {
+        for ip in interface.ips {
+            if let IpNetwork::V4(ipv4) = ip {
+                return Some(IpAddr::V4(ipv4.ip()));
+            }
+        }
+    }
+    None
+}
